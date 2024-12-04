1b69e0957ccc823413aaaf0a72920a172010faf7
MD PERWEJ ALAM <98713406+perwejalam03@users.noreply.github.com>
2024-12-04 06:42:41 +0000

    datatypes

---
+++ basic_01/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3+3)  // we are using node js not browser
+
+console.log(3
+    +3
+)  // code readability should be high
+
+console.log("Alam");
+
+
+// number 2 to power 53
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol unique
+
+// object
+console.log(typeof undefined);   // undefined
+console.log(typeof null);  // object
+
