33a899a7b8e330ed60202ec38baf11c934a4bcac
MD PERWEJ ALAM <98713406+perwejalam03@users.noreply.github.com>
2024-12-04 06:26:35 +0000

    variables discussion

---
+++ basic_01/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "alam@gmail.com"
+var accountPassword = "alam123"
+accountCity = "Katihar"
+let country;
+// accountId = 5;  // const can't re-initialize
+console.log(accountId);
+
+accountEmail = "new@gmail.com"
+accountPassword = "new123"
+accountCity = "new City"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, country])  // for tabular structure
+
