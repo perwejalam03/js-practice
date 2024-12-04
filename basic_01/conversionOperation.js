596adf9681e1c66e952c0ea67cd03b81ceb95dc4
MD PERWEJ ALAM <98713406+perwejalam03@users.noreply.github.com>
2024-12-04 06:56:41 +0000

    conversion of datatypes

---
+++ basic_01/conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "25";
+
+console.log(typeof score);  // string
+console.log(typeof(score)); // string
+
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber);
+
+// "25" => 25
+// "25ad" => NaN
+// true => 1, false => 0
+
+
+
+
